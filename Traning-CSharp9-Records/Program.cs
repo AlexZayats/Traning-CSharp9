7b5a0fd5a4688fae0b6361b1922b250c50b0a193
Aliaksandr Zayats <a.zayats@outlook.com>
2020-10-18 15:21:50 +0300

    Records sample
    Add project with Records semple.

---
+++ Traning-CSharp9-Records/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Traning_CSharp9_Records
+{
+    /*
+    public record Person
+    {
+        public string LastName { get; }
+        public string FirstName { get; }
+
+        public Person(string first, string last) => (FirstName, LastName) = (first, last);
+    }
+    */
+
+    public record Person(string FirstName, string LastName);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new("Paul", "McCartney");
+            Person p2 = new("Paul", "McCartney");
+
+            Console.WriteLine(p1 == p2);
+
+            Person p3 = p2 with { FirstName = "Brown" };
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+
+            var (first, last) = p1;
+            Console.WriteLine(first);
+            Console.WriteLine(last);
+        }
+    }
+}
